9a3b24d3afa37b56051e788cac0f8cbf04f3b7a9
miaoyu <miaoyu@sefonsoft.com>
2020-12-15 16:41:53 +0800

    init

---
+++ client/src/app.ts
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Taro, { Config } from '@tarojs/taro'
+
+import './app.less'
+
+class App extends Component {
+
+  componentDidMount () {
+    if (process.env.TARO_ENV === 'weapp') {
+      Taro.cloud.init({
+        // env 参数说明：
+        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
+        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
+        //   如不填则使用默认环境（第一个创建的环境）
+        env: 'wcloud-7gq3rs6qb5693239',
+        traceUser: true,
+      })
+    }
+  }
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // this.props.children 是将要会渲染的页面
+  render () {
+    return this.props.children
+  }
+}
+
+export default App
