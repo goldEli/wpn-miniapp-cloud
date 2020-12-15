4599eaf1452a6c998426e491cbc53f3dd2084db2
miaoyu <miaoyu@sefonsoft.com>
2020-12-15 20:50:57 +0800

    modify price fontsize

--- client/src/pages/index/components/Content/index.weapp.tsx
+++ client/src/pages/index/components/Content/index.weapp.tsx
@@ -21,7 +21,7 @@ export default class Content extends Component {
               <View key={idx.toString()} className="item">
                 <Image className="img" mode="widthFix" src="https://wx4.sinaimg.cn/mw690/671cf50fgy1gloqtxr025j20500500sm.jpg"></Image>
                 <View className="title">杨老师的房间阿里山啊收到了房间阿里山士大夫啊收到了附件</View>
-                <View className="price">￥{idx}</View>
+                <View className="price">￥{idx}/串/200g</View>
                 <View className="action">
                   <View className="button-icon-sub">
                     <View className='at-icon at-icon-subtract'></View>
