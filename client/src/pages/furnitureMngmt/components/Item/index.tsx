c1629ae646a66779d9de7d095f710cb84f1a4062
miaoyu <miaoyu@sefonsoft.com>
2021-02-01 14:14:30 +0800

    chore: add divider

--- client/src/pages/furnitureMngmt/components/Item/index.tsx
+++ client/src/pages/furnitureMngmt/components/Item/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Taro from "@tarojs/taro";
 import { View, Image } from "@tarojs/components";
-import { AtCard, AtTextarea, AtSwitch } from "taro-ui";
+import { AtCard, AtDivider, AtSwitch } from "taro-ui";
 import ImageBox from "../ImageBox";
 
 import Title from "@/components/Title";
@@ -9,11 +9,9 @@ interface IItemProps {}
 
 const Item: React.FC<IItemProps> = props => {
   const edit = () => {
-    console.log("edit")
-  }
-  const onChangeSwitch = () => {
-
-  }
+    console.log("edit");
+  };
+  const onChangeSwitch = () => {};
   return (
     <View className="furnitureMngomt-item">
       <AtCard onClick={edit} title="a系列茶几" note="排序1，￥3000">
@@ -27,9 +25,8 @@ const Item: React.FC<IItemProps> = props => {
           规格：C-120(茶)1200*600*465
           \n配色大胆，符合现今大众审美。油漆符合环保要求，无刺激性气味，板材不易受潮变形，抽屉采用实木，质地耐磨。
         </View>
-        <View>
-          <AtSwitch title='是否上架' checked={true} onChange={onChangeSwitch} />
-        </View>
+        <AtDivider />
+        <AtSwitch title="是否上架" checked={true} onChange={onChangeSwitch} />
       </AtCard>
     </View>
   );
