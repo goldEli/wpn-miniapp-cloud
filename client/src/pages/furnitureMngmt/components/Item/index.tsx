a4ec2f1e2f597488dba88aaa1d0344cf5dae92be
miaoyu <miaoyu@sefonsoft.com>
2021-02-01 17:47:17 +0800

    chore: adjust note show

--- client/src/pages/furnitureMngmt/components/Item/index.tsx
+++ client/src/pages/furnitureMngmt/components/Item/index.tsx
@@ -3,54 +3,31 @@ import Taro from "@tarojs/taro";
 import { View, Image } from "@tarojs/components";
 import { AtCard, AtDivider, AtSwitch, AtButton } from "taro-ui";
 import ImageBox from "../ImageBox";
-import "./index.less"
+import "./index.less";
 import { IFurniture } from "@/type";
 import { open } from "../UpdateFurnitureModal";
 
 interface IItemProps {
-  data: IFurniture
+  data: IFurniture;
 }
 
 const Item: React.FC<IItemProps> = props => {
-  const {data} = props
-  
+  const { data } = props;
+
   const edit = () => {
-    open(data)
+    open(data);
   };
-  const note = `排序${data.index}，￥${data.price}，${data.onSale ? "已上架" : "未上架"}`
-  const imageSrcList = data.imgSrc?.split(",").filter(item => item) || []
+  const note = `排序${data.index}，￥${data.price}，${
+    data.onSale ? "已上架" : "未上架"
+  }`;
+  const imageSrcList = data.imgSrc?.split(",").filter(item => item) || [];
   return (
     <View className="furnitureMngomt-item">
       <AtCard onClick={edit} title={data.title} note={note}>
-        <ImageBox
-          list={imageSrcList}
-        />
-        <View>
-          {data.note}
-        </View>
-        {/* <AtDivider />
-        <AtSwitch title="是否上架" checked={true} onChange={onChangeSwitch} />
-        <AtDivider />
-        <View className="item-button-box">
-          <View className="wme-small-btn">
-            <AtButton
-              circle
-              type="secondary"
-              full={false}
-            >
-              删除
-            </AtButton>
-          </View>
-          <View className="wme-small-btn">
-            <AtButton
-              circle
-              type="primary"
-              full={false}
-            >
-              修改
-            </AtButton>
-          </View>
-        </View> */}
+        <ImageBox list={imageSrcList} />
+        {data.note?.split("\n").map(item => {
+          return <View>{item}</View>;
+        })}
       </AtCard>
     </View>
   );
