62fc871feb354337be213e23ea842af191723950
miaoyu <miaoyu@sefonsoft.com>
2021-02-02 10:36:47 +0800

    chore: modify index router

--- client/src/pages/index/index.tsx
+++ client/src/pages/index/index.tsx
@@ -1,39 +1,30 @@
-import React, { Component } from 'react'
-import Taro, { Config } from '@tarojs/taro'
-import { Button } from '@tarojs/components'
-import Home from "./home";
+import React, { Component } from "react";
+import Taro, { useShareAppMessage } from "@tarojs/taro";
+import { View } from "@tarojs/components";
+import Content from "./components/Content/index.weapp";
+import Footer from "./components/Footer/index.weapp";
+import "./index.less";
+import { MenuContextProvider } from "./context/MenuContext";
 
-export default class Index extends Component {
-  state = {
-    showOrder: false
-  }
-
-
-  onShareAppMessage(res) { //放在父组件上执行，子组件上不被执行！
-    if (res.from === 'button') {
-      this.setState({
-        showOrder: true
-      })
-      // 来自页面内转发按钮
-      console.log(res.target)
-      const { sum, text } = res?.target?.dataset || {}
-      return {
-        title: `我的订单(共计${sum}元)`,
-        path: `pages/order/index?text=${text}`
-      }
-    }
-    return {
-      title: '点击进入',
-      path: 'pages/index/index'
-    }
-  }
-
-  render() {
-    return (
-      <>
-        {this.state.showOrder && <Button onClick={() => this.setState({ showOrder: false })} size="mini">返回首页</Button>}
-        <Home showOrder={this.state.showOrder} />
-      </>
-    )
-  }
+interface IIndexProps {
 }
+
+const Index: React.FC<IIndexProps> = props => {
+  useShareAppMessage(res => {
+    return {
+      title: "点击进入",
+      path: "pages/index/index"
+    };
+  });
+  return (
+    <MenuContextProvider>
+      <View className="wme-index">
+        <Content />
+        <Footer />
+      </View>
+    </MenuContextProvider>
+  );
+};
+
+Index.enableShareAppMessage = true;
+export default Index;
