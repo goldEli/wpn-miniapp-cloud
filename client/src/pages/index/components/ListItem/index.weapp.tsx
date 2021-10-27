4b84fb5eff4cee83a30a8c45b36d06c918ce0956
miaoyu <miaoyu2009@qq.com>
2021-10-27 19:34:18 +0800

    💄btn position

--- client/src/pages/index/components/ListItem/index.weapp.tsx
+++ client/src/pages/index/components/ListItem/index.weapp.tsx
@@ -41,33 +41,35 @@ const Item: React.FC<IItemProps> = props => {
         )}
       </View>
       <View className="action">
-        {(data.number as number) > 0 && (
-          <>
-            <View
-              onClick={(e: any) => {
-                e.stopPropagation();
-                action?.sub?.(data._id || "");
-              }}
-              className="button-icon-sub"
-            >
-              <View className="button-icon-sub-bg">
-                <View className="at-icon at-icon-subtract"></View>
+        <View className="action-fix">
+          {(data.number as number) > 0 && (
+            <>
+              <View
+                onClick={(e: any) => {
+                  e.stopPropagation();
+                  action?.sub?.(data._id || "");
+                }}
+                className="button-icon-sub"
+              >
+                <View className="button-icon-sub-bg">
+                  <View className="at-icon at-icon-subtract"></View>
+                </View>
               </View>
+              <View onClick={changeNumber} className="text">
+                {data.number}
+              </View>
+            </>
+          )}
+          <View
+            onClick={(e: any) => {
+              e.stopPropagation();
+              action?.plus?.(data._id || "");
+            }}
+            className="button-icon-add"
+          >
+            <View className="button-icon-add-bg">
+              <View className="at-icon at-icon-add"></View>
             </View>
-            <View onClick={changeNumber} className="text">
-              {data.number}
-            </View>
-          </>
-        )}
-        <View
-          onClick={(e: any) => {
-            e.stopPropagation();
-            action?.plus?.(data._id || "");
-          }}
-          className="button-icon-add"
-        >
-          <View className="button-icon-add-bg">
-            <View className="at-icon at-icon-add"></View>
           </View>
         </View>
       </View>
